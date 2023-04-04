8d73708caa8f873e263ea29b5e05c29024bcb5c2
wolftodiawa <tobias.wolf@diawa.at>
2023-04-04 09:43:41 +0200

    Adds demo to reproduce the issue

---
+++ BlazorProject1/Client/ClassName.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorProject1.Client
+{
+  public class Entity
+  {
+    public Guid Id { get; set; }
+  }
+
+  public class ClassName : Entity
+  {
+    public string Name { get; set; }
+    public string Value { get; set; }
+    public string Description { get; set; }
+    public bool BoolValue { get; set; }
+  }
+}
